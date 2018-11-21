a8b57a792b81a120989b5e12b452df3aaf0acacf
SamwelOpiyo <samonyango20@gmail.com>
2018-11-21 22:50:31 +0000

    Write Product of alphabet characters with a repeat of 2 to file.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
 #[macro_use] extern crate itertools;
 
 fn main() {
     //use itertools::Itertools;
     let char = "abcdefghijklmnopqrstuvwxyz";
-    for (i, j, k, l, n, m, o) in iproduct!(char.chars(), char.chars(), char.chars(), char.chars(), char.chars(), char.chars(), char.chars()) {
-       println!("{}{}{}{}{}{}{}", i,j,k,l,n,m,o)
+    let f = File::create("product_alphabet_3.txt").expect(
+        "Unable to create file"
+    );
+    let mut f = BufWriter::new(f);
+    for (i, j, k) in iproduct!(
+        char.chars(), char.chars(), char.chars() 
+    ) {
+       let mut line = String::with_capacity(5);
+       line.push(i);
+       line.push(j);
+       line.push(k);
+       line.push_str("\n");
+       f.write_all(line.as_bytes()).expect(
+           "Unable to write data"
+       );
     }
 }
